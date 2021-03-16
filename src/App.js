d8371f83d1a32735b9f0aa51d16fd96620ec3b86
xpmprd <xpmprd1@gmail.com>
2021-03-16 22:04:21 +0100

    fffffff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'; 
 import { HomePage } from './pages/HomePage/HomePage';  
-import { Menu } from './pages/Menu/Menu';
+ 
 
 const App = () => (
   <Router>
